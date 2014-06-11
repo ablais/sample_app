9966db0070bc0fb22087679c61c9a00460d1a5e4
Andre <ror@teststation2.(none)>
2014-06-11 15:00:46 +0000

    Complete James first tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @count = User.count
+    @usernum = @user["id"]
   end
   def new
     @user = User.new
